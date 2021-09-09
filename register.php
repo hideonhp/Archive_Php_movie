98cd623c48f2199177038caea2ccecf7b1e382ad
HideOnHp <55818538+hideonhp@users.noreply.github.com>
2021-09-10 01:39:14 +0700

    Add: Register File

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+     <title>Đăng Ký Tài Khoản</title>
+     <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+
+<body>
+     <div class="sICon">
+          <div class="col"></div>
+     </div>
+</body>
+
+</html>
